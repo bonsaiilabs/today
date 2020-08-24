eb3c94a507b527302a2e0ceb11c625a748e0e6ba
hhimanshu <harit.himanshu@yahoo.in>
2020-08-24 12:46:29 -0700

    Add prettier, husky, lint-staged for code formatting

--- src/ToDoApp.test.tsx
+++ src/ToDoApp.test.tsx
@@ -1,48 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import {cleanup, render, screen} from '@testing-library/react'
-import {ToDoApp} from "./ToDoApp";
-import userEvent from '@testing-library/user-event'
+import { cleanup, render, screen } from "@testing-library/react";
+import { ToDoApp } from "./ToDoApp";
+import userEvent from "@testing-library/user-event";
 
+afterEach(cleanup);
 
-afterEach(cleanup)
+describe("When the page initially loaded", () => {
+  it('renders the "My Dashboard" title on the main page', () => {
+    const { queryByText } = render(<ToDoApp />);
+    expect(queryByText(/My Dashboard/i)).toBeInTheDocument();
+  });
 
-describe('When the page initially loaded', () => {
-    it('renders the "My Dashboard" title on the main page', () => {
-        const {queryByText} = render(<ToDoApp/>)
-        expect(queryByText(/My Dashboard/i)).toBeInTheDocument()
-    })
+  it("renders the form where todo can be added", () => {
+    const { container, getByPlaceholderText, getByRole } = render(<ToDoApp />);
+    expect(container.querySelector("#todo-form")).toBeVisible();
+    expect(getByPlaceholderText("Enter your task")).toBeVisible();
+    expect(getByRole("button")).toBeVisible();
+  });
 
-    it('renders the form where todo can be added', () => {
-        const {container, getByPlaceholderText, getByRole} = render(<ToDoApp/>)
-        expect(container.querySelector('#todo-form')).toBeVisible()
-        expect(getByPlaceholderText('Enter your task')).toBeVisible()
-        expect(getByRole('button')).toBeVisible()
-    })
+  it("renders all the todos in a section", () => {
+    const { container } = render(<ToDoApp />);
+    expect(container.querySelector("#all-todos")).toBeVisible();
+  });
+});
 
-    it('renders all the todos in a section', () => {
-        const {container} = render(<ToDoApp/>)
-        expect(container.querySelector('#all-todos')).toBeVisible()
-    })
-})
+describe("When a user interacts with the form", () => {
+  // https://github.com/testing-library/user-event#typeelement-text-options
+  it("fills the text box when user provides the input", () => {
+    render(<ToDoApp />);
 
-describe('When a user interacts with the form', () => {
-    // https://github.com/testing-library/user-event#typeelement-text-options
-    it('fills the text box when user provides the input', () => {
-        render(<ToDoApp/>)
+    const todo: string = "Finish the test";
+    userEvent.type(screen.getByRole("textbox"), todo);
+    expect(screen.getByRole("textbox")).toHaveValue(todo);
+  });
 
-        const todo: string = "Finish the test";
-        userEvent.type(screen.getByRole('textbox'), todo)
-        expect(screen.getByRole('textbox')).toHaveValue(todo)
-    })
+  it('adds a todo when a user clicks on the "ADD" button', () => {
+    render(<ToDoApp />);
+    const todo: string = "first task";
+    userEvent.type(screen.getByRole("textbox"), todo);
+    expect(screen.getByRole("textbox")).toHaveValue(todo);
 
-    it('adds a todo when a user clicks on the "ADD" button', () => {
-        render(<ToDoApp/>)
-        const todo: string = "first task"
-        userEvent.type(screen.getByRole('textbox'), todo)
-        expect(screen.getByRole('textbox')).toHaveValue(todo)
-
-        userEvent.click(screen.getByRole("button"))
-        expect(screen.getByRole('textbox')).toBeEmptyDOMElement()
-    })
-})
+    userEvent.click(screen.getByRole("button"));
+    expect(screen.getByRole("textbox")).toBeEmptyDOMElement();
+  });
+});
--- src/ToDoApp.tsx
+++ src/ToDoApp.tsx
@@ -1,28 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const ToDoApp: React.FC = () => {
-    const EMPTY_TODO: string = "";
-    const [todos, setTodos] = useState<string[]>([]);
-    const [todoText, setTodoText] = useState<string>(EMPTY_TODO)
+  const EMPTY_TODO: string = "";
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todoText, setTodoText] = useState<string>(EMPTY_TODO);
 
-    const onTextChange = (e: React.FormEvent<HTMLInputElement>): void => setTodoText(e.currentTarget.value);
-    const onAddClick = (e: React.FormEvent<HTMLButtonElement>): void => {
-        e.preventDefault();
-        setTodos(currentToDos => [...currentToDos, todoText]);
-        setTodoText(EMPTY_TODO);
-    }
+  const onTextChange = (e: React.FormEvent<HTMLInputElement>): void =>
+    setTodoText(e.currentTarget.value);
+  const onAddClick = (e: React.FormEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    setTodos((currentToDos) => [...currentToDos, todoText]);
+    setTodoText(EMPTY_TODO);
+  };
 
-    return <div id={'todo-app'}>
-        <h1>My Dashboard</h1>
-        <form id={'todo-form'}>
-            <input type={'text'} placeholder={'Enter your task'} value={todoText} onChange={onTextChange}/>
-            <button onClick={onAddClick}>ADD</button>
-        </form>
-        <div id={'all-todos'}>
-            <h1>All ToDos</h1>
-            <div>
-                {todos.map((todo, key) => <p key={key}>{todo}</p>)}
-            </div>
+  return (
+    <div id={"todo-app"}>
+      <h1>My Dashboard</h1>
+      <form id={"todo-form"}>
+        <input
+          type={"text"}
+          placeholder={"Enter your task"}
+          value={todoText}
+          onChange={onTextChange}
+        />
+        <button onClick={onAddClick}>ADD</button>
+      </form>
+      <div id={"all-todos"}>
+        <h1>All ToDos</h1>
+        <div>
+          {todos.map((todo, key) => (
+            <p key={key}>{todo}</p>
+          ))}
         </div>
+      </div>
     </div>
-}
+  );
+};
