721b4c687d88c9052197ec938fd8e3b0acfba9bc
hhimanshu <harit.himanshu@yahoo.in>
2020-08-24 06:50:53 -0700

    The tests when page is initially loaded

--- src/ToDoApp.test.tsx
+++ src/ToDoApp.test.tsx
@@ -5,7 +5,21 @@ import {ToDoApp} from "./ToDoApp";
 
 afterEach(cleanup)
 
-it('renders the "My Dashboard" title on the main page', ()=> {
-    const {queryByText} = render(<ToDoApp/>)
-    expect(queryByText(/My Dashboard/i)).toBeInTheDocument()
+describe('When the page initially loaded', () => {
+    it('renders the "My Dashboard" title on the main page', ()=> {
+        const {queryByText} = render(<ToDoApp/>)
+        expect(queryByText(/My Dashboard/i)).toBeInTheDocument()
+    })
+
+    it('renders the form where todo can be added', () => {
+        const {container, getByPlaceholderText, getByRole} = render(<ToDoApp/>)
+        expect(container.querySelector('#todo-form')).toBeVisible()
+        expect(getByPlaceholderText('Enter your task')).toBeVisible()
+        expect(getByRole('button')).toBeVisible()
+    })
+
+    it('renders all the todos in a section', () => {
+        const {container} = render(<ToDoApp/>)
+        expect(container.querySelector('#all-todos')).toBeVisible()
+    })
 })
