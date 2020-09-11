7985a342f34a2aca3778aa6eccc005bc204f3dca
hhimanshu <harit.himanshu@yahoo.in>
2020-09-11 06:06:06 -0700

    Footer as per the design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,7 @@ import styles from "./Footer.module.css";
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <div>
-        <a href={"/"}>How it works?</a>
-      </div>
+      <button>Learn how it works?</button>
     </footer>
   );
 };
