dcf6277a980b6a2b498798c54cb778c3a6b6fd2c
hhimanshu <harit.himanshu@yahoo.in>
2020-09-11 10:41:52 -0700

    One Goal and 1 message

--- src/components/TodaysGoals.tsx
+++ src/components/TodaysGoals.tsx
@@ -6,18 +6,29 @@ interface TodaysGoalsProps {
   todos: ToDos;
 }
 
+const MAX_GOALS_ALLOWED = 3;
+
 const TodaysGoals: ({ todos }: TodaysGoalsProps) => JSX.Element = ({
   todos,
 }) => {
+  const numGoalsRemaining = MAX_GOALS_ALLOWED - todos.all.length;
   return (
     <div className={styles.goalsContainer}>
       <div id={"all-todos"}>
         {todos.all.map((todo, key) => (
-          <div key={key}>
+          <div key={key} className={styles.todoRow}>
             <input type={"checkbox"} />
             <span>{todo.text}</span>
+            <button>START</button>
           </div>
         ))}
+      </div>
+      <div className={styles.messageContainer}>
+        <p>
+          You can add {numGoalsRemaining} more goals for today, but it is not
+          required. <i>Less is more. Focus on completing 1 task well</i>
+        </p>
+        <button>Add a goal for today</button>
       </div>
     </div>
   );
